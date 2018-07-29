6b6d019efc684cbfd5650acc48033a2c03ade1d6
Kevin Townsend <ktownsend@eab.com>
2018-07-29 19:50:51 -0400

    added pitch me

--- skills/sith_post.js
+++ skills/sith_post.js
@@ -1,18 +1,7 @@
-/*
-
-WHAT IS THIS?
-
-This module demonstrates simple uses of Botkit's `hears` handler functions.
-
-In these examples, Botkit is configured to listen for certain phrases, and then
-respond immediately with a single line response.
-
-*/
-
-var wordfilter = require('wordfilter');
-var _ = require('lodash');
-var moment = require('moment');
-var { starWarsLines } = require('../data/quotes.js');
+const wordfilter = require('wordfilter');
+const _ = require('lodash');
+const moment = require('moment');
+const { starWarsLines } = require('../data/quotes.js');
 
 module.exports = function(controller) {
   
